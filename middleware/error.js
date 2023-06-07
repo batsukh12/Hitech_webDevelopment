518fcafcb9f135d55a1a3c3a12a3cc6350cec64c
batsukh12 <112690266+batsukh12@users.noreply.github.com>
2023-06-07 17:41:33 +0800

    Add files via upload

---
+++ middleware/error.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+    console.log("ErrorStatus", err.status || 500);
+  console.log("ErrorMessage", err.message);
+
+  res.status(err.status || 500).json({
+    success: false,
+    status: err.status || 500,
+    message: err.message,
+  });
+};
+
+module.exports = errorHandler;
