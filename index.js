ea928936737dc193d2871eaa02412d17c4f89703
batsukh12 <112690266+batsukh12@users.noreply.github.com>
2023-06-07 17:43:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const app = express();
 const PORT = 3000;
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user-routes");
+const errorHandler = require("./middleware/error");
+
+app.get("/", (req, res, next) => {
+  try {
+    throw new Error("Aldaa garlaa");
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorHandler);
 
 connectDB();
+
 
 app.use(express.json());
 app.use("/api/v1/auth", userRoutes);
