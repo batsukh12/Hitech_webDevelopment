20dfe0c0ea804dd21a875a97f797a04b5eda0280
batsukh12 <112690266+batsukh12@users.noreply.github.com>
2023-06-08 15:20:11 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const app = express();
-const PORT = 3000;
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user-routes");
 const errorHandler = require("./middleware/error");
+const categoryRoutes = require("./routes/category-routes")
+const subCatRoutes = require("./routes/subCategory-routes")
+const productRoutes = require("./routes/product-routes")
+const orderRoutes = require("./routes/order-routes")
 
+
+const app = express();
+const PORT = 3000;
 app.get("/", (req, res, next) => {
   try {
     throw new Error("Aldaa garlaa");
@@ -13,13 +18,24 @@ app.get("/", (req, res, next) => {
   }
 });
 
-app.use(errorHandler);
 
 connectDB();
-
-
+// user routes
 app.use(express.json());
 app.use("/api/v1/auth", userRoutes);
+// category routes
+app.use("/api/admin", categoryRoutes);
+// subcategory routes
+app.use("/api/v2", subCatRoutes);
+//product routes
+app.use("/api/product", productRoutes);
+// order routes
+app.use("/api/order", orderRoutes);
+
+
+app.use(errorHandler);
+
+
 
 app.listen(PORT, (err) => {
   console.log("Express running on port " + PORT);
